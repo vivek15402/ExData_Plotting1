f2c93fb3dc95ae3a6787918c9ed8ec9501198f38
vivek15402 <44522137+vivek15402@users.noreply.github.com>
2020-03-30 07:32:40 +0530

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -13,6 +13,8 @@ subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%
 # initiating a composite plot with many graphs
 par(mfrow=c(2,2))
 
+png("plot4.png", width=480, height=480)
+
 # calling the basic plot function that calls different plot functions to build the 4 plots that form the graph
 with(subpower,{
   plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",  xlab="",ylab="Global Active Power")  
@@ -24,3 +26,5 @@ with(subpower,{
    legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), cex = 0.6)
   plot(subpower$Time,as.numeric(as.character(subpower$Global_reactive_power)),type="l",xlab="datetime",ylab="Global_reactive_power")
 })
+
+dev.off()
