ce7c8455cb67162dde2a7c50f3973299090069b2
vivek15402 <44522137+vivek15402@users.noreply.github.com>
2020-03-30 07:32:20 +0530

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -9,6 +9,7 @@ subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
 subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
+png("plot3.png", width=480, height=480)
 
 # calling the basic plot functions
 plot(subpower$Time,subpower$Sub_metering_1,type="n",xlab="",ylab="Energy sub metering")
@@ -19,3 +20,5 @@ legend("topright", lty=1, col=c("black","red","blue"),legend=c("Sub_metering_1",
 
 # annotating graph
 title(main="Energy sub-metering")
+
+dev.off()
