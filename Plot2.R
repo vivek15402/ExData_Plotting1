604e9e68b7013622ba8fea56d5206cb8e6170b1b
vivek15402 <44522137+vivek15402@users.noreply.github.com>
2020-03-30 07:31:56 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -9,9 +9,12 @@ subpower$Time <- strptime(subpower$Time, format="%H:%M:%S")
 subpower[1:1440,"Time"] <- format(subpower[1:1440,"Time"],"2007-02-01 %H:%M:%S")
 subpower[1441:2880,"Time"] <- format(subpower[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
 
+png("plot2.png", width=480, height=480)
 
 # calling the basic plot function
 plot(subpower$Time,as.numeric(as.character(subpower$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
 
 # annotating graph
 title(main="Global Active Power Vs Time")
+
+dev.off()
