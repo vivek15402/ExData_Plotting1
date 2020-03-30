32a9ba41aec3ad476fbf05cf8897fb8d4a248201
vivek15402 <44522137+vivek15402@users.noreply.github.com>
2020-03-30 07:31:30 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -3,8 +3,12 @@ power <- read.table("household_power_consumption.txt",skip=1,sep=";")
 names(power) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 subpower <- subset(power,power$Date=="1/2/2007" | power$Date =="2/2/2007")
 
+png("plot1.png", width=480, height=480)
+
 #calling the basic plot function
 hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
 
 # annotating graph
 title(main="Global Active Power")
+
+dev.off()
